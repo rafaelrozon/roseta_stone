2077aeda4f29658854c36e436db9a74a7c123cef
Rafael Rozon <rafaelrozon.developer@gmail.com>
2019-05-09 21:10:13 -0700

    initial files

---
+++ js/BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,18 @@
+class BST {
+    // add
+    // remove
+    // find
+    preOrderTraversal(node) {
+
+    }
+
+    postOrderTraversal(node) {
+
+    }
+
+    inOrderTraversal(node) {
+
+    }
+}
+
+module.exports = BST;
---
+++ js/BinarySearchTree/Node.js
@@ -0,0 +1,9 @@
+class Node {
+    constructor(val, lft = null, rgt = null) {
+        this.val = val
+        this.lft = lft;
+        this.rgt = rgt;
+    }
+}
+
+module.exports = Node;
